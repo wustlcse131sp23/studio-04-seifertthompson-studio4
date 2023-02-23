8e3c52606f2b1c4871c39b420b0292fc9932606a
BJT12 <BJT12@bjt1229.dhcp.wustl.edu>
2023-02-23 12:25:07 -0600

    InterpretDrawingFile

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,50 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String type = in.next();
+		
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		boolean isFilled = in.nextBoolean();
+		
+		double centerPosX = in.nextDouble();
+		double centerPosY = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		System.out.println(type);
+		if(type.equals("rectangle") && isFilled == true) {
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.filledRectangle(centerPosX, centerPosY, halfWidth, halfHeight);
+		}
+		else if(type.equals("rectangle")) {
+			StdDraw.rectangle(centerPosX, centerPosY, halfWidth, halfHeight);
+		}
+		else if(type.equals("ellipse") && isFilled == true) {
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.filledEllipse(centerPosX, centerPosY, halfWidth, halfHeight);
+		}
+		else if(type.equals("ellipse")) {
+			StdDraw.filledEllipse(centerPosX, centerPosY, halfWidth, halfHeight);
+		}
+		
+		if(in.hasNextDouble())
+		{
+			double pointXFinal = in.nextDouble();
+			double pointYFinal = in.nextDouble();
+			double[] pointX = {centerPosX, halfWidth, pointXFinal};
+			double[] pointY = {centerPosY, halfHeight, pointYFinal};
+			
+			if(isFilled == true) {
+				StdDraw.setPenColor(red, green, blue);
+				StdDraw.filledPolygon(pointX, pointY);
+			}
+			else
+			{
+				StdDraw.polygon(pointX, pointY);
+			}
+		}
 	}
 }
